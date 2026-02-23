b49533c77281e0a5a6714ba7a795edad87f443d8
Christophe72 <christophe.seyler@hotmail.com>
2026-02-23 19:20:11 +0100

    correction Long en UUid

--- backend/src/main/java/com/webelec/backend/navigation/repository/UserCompanyAssignmentRepository.java
+++ backend/src/main/java/com/webelec/backend/navigation/repository/UserCompanyAssignmentRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserCompanyAssignmentRepository extends JpaRepository<UserCompanyAssignment, Long> {
+public interface UserCompanyAssignmentRepository extends JpaRepository<UserCompanyAssignment, java.util.UUID> {
 
     Optional<UserCompanyAssignment> findByUserId(Long userId);
 }
